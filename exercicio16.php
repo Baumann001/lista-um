38b03df4a8661319f6912ff74e48fd594da80b09
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-07-14 22:25:37 -0300

    add-

---
+++ exercicio16.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <form method="POST">
+    <input type="number" name="num1" placeholder="Idade" required>
+    <input type="text" name="txt" placeholder="nome" required>
+    <button type="submit" name="verificar">Verificar</button>
+</form>
+
+<?php
+if (isset($_POST['verificar'])) {
+    $num1 = $_POST['num1'];
+    $txt = $_POST['txt'];
+
+    if($num1 > 16 ){
+        echo "Voce  pode votar $txt!";
+    }elseif($num1 <16 ){
+    echo "Voce não pode votar $txt!";
+    }else{
+    echo "voce pode votar!"
+    }
+
+
+}
+
+?>
+</body>
+</html>
